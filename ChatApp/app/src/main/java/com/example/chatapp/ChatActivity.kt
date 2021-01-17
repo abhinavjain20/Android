0d1a71990e9d82eba45b3ba50d03f3e1bd776f4b
abhinavjain20 <jainabhinav568@gmail.com>
2021-01-17 17:33:43 +0530

    Adding Logout and ChatUI

---
+++ ChatApp/app/src/main/java/com/example/chatapp/ChatActivity.kt
@@ -0,0 +1,32 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class ChatActivity : AppCompatActivity() {
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.chat_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            auth.signOut()
+            Toast.makeText(this, "LogOut Successfully", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
+++ ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.chatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +17,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-        setContentView(R.layout.activity_main)
         _binding.loginBtn.setOnClickListener {
-            auth.createUserWithEmailAndPassword(_binding.email.text.toString(), _binding.pass.text.toString())
-                .addOnSuccessListener { Toast.makeText(this,"Logged in Successfully",Toast.LENGTH_LONG).show()}
-                .addOnFailureListener { Toast.makeText(this,"${it.localizedMessage}",Toast.LENGTH_LONG).show() }
+            auth.createUserWithEmailAndPassword(
+                _binding.email.text.takeIf { it!!.isNotBlank() }.toString(),
+                _binding.pass.text.takeIf { it!!.isNotBlank() }.toString()
+            )
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Account Created Successfully ", Toast.LENGTH_SHORT).show()
+                    val profileUpdate = UserProfileChangeRequest.Builder()
+                        .setDisplayName(_binding.username.toString())
+                        .build()
+                    it.user?.updateProfile(profileUpdate)
+                }
+                .addOnFailureListener {
+                    if (it.localizedMessage.contains("already", true)) {
+                        signUp(_binding.email.text.toString(), _binding.pass.text.toString())
+                    } else {
+                        when {
+                            _binding.email.text.toString().isEmpty() -> {
+                                Toast.makeText(this, "Email cannot be empty", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                            _binding.pass.text.toString().isEmpty() -> {
+                                Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                            else -> {
+                                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG)
+                                    .show()
+                            }
+                        }
+                    }
+                }
         }
+
+        auth.addAuthStateListener {
+            if (it.currentUser != null) {
+                startActivity(Intent(this, ChatActivity::class.java))
+            }
+        }
+    }
+
+    private fun signUp(email: String, pass: String) {
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    it.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .addOnSuccessListener {
+                Toast.makeText(
+                    this,
+                    "Logged In Successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
